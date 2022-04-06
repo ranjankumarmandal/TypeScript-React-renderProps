c1ea2c27259d08336fa788b22f32ed0f08d64d19
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-06 17:33:02 +0530

    created a state count with setCount()

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   render: () => JSX.Element;
 }
 
 const Counter = (props: Props) => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <p>counter</p>
