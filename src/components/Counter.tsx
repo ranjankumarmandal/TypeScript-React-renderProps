b4786cb10dfc55b0bb6f4fd9162788ff48a5d095
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-06 18:39:33 +0530

    defined types of render and placed it in side div tag in component Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 
 interface Props {
-  render: () => JSX.Element;
+  render: (
+    count: number,
+    setCount: React.Dispatch<React.SetStateAction<number>>
+  ) => JSX.Element;
 }
 
 const Counter = (props: Props) => {
   const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <p>counter</p>
-    </div>
-  );
+  return <div>{props.render(count, setCount)}</div>;
 };
 
 export default Counter;
